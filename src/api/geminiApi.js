6b2938faf754f01f45e86dedbd77c66a03a1aed6
MayurSatone <mayursatone08@gmail.com>
2025-05-11 17:13:32 +0530

    Update geminiApi.js

--- src/api/geminiApi.js
+++ src/api/geminiApi.js
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = 'AIzaSyATQYkbehVhfAikgg3WKR9clxivj_60BkA'; // Remove this in production!
+const GEMINI_API_KEY = ''; // Remove this in production!
 const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent" ;
 
 export const generateContentWithGemini = async (prompt) => {
